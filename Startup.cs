6328982501dce2cf054e2f597f2673a1e342f347
Andrea Lepore <andrea@andrealepore.com>
2020-10-12 15:22:01 +0200

    Aggiunto ASP.NET Core MVC

--- Startup.cs
+++ Startup.cs
@@ -16,6 +16,8 @@ namespace MyCourse
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc(option => option.EnableEndpointRouting = false);
+            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -26,14 +28,10 @@ namespace MyCourse
                 app.UseDeveloperExceptionPage();
             }
             app.UseStaticFiles();
-            app.UseRouting();
-
-            app.UseEndpoints(endpoints =>
+            app.UseMvcWithDefaultRoute();
+            app.UseMvc(routeBuilder =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                routeBuilder.MapRoute("default", "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
